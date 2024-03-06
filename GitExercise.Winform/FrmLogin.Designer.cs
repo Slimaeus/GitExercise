75e8fe7e16b9357af7251c6aabca24ae02944d0d
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-03-06 13:29:06 +0700

    feat: Edit Password label and text box - Add login button

--- GitExercise.Winform/FrmLogin.Designer.cs
+++ GitExercise.Winform/FrmLogin.Designer.cs
@@ -30,53 +30,63 @@ partial class FrmLogin
     {
         label1 = new Label();
         txtUserName = new TextBox();
-        txtmatkhau = new TextBox();
+        txtpassword = new TextBox();
         label2 = new Label();
+        btnlogin = new Button();
         SuspendLayout();
         // 
         // label1
         // 
         label1.AutoSize = true;
-        label1.Location = new Point(34, 55);
+        label1.Location = new Point(30, 41);
         label1.Name = "label1";
-        label1.Size = new Size(78, 20);
+        label1.Size = new Size(62, 15);
         label1.TabIndex = 0;
         label1.Text = "UserName";
         // 
         // txtUserName
         // 
-        txtUserName.Location = new Point(112, 51);
-        txtUserName.Margin = new Padding(3, 4, 3, 4);
+        txtUserName.Location = new Point(98, 38);
         txtUserName.Name = "txtUserName";
-        txtUserName.Size = new Size(270, 27);
+        txtUserName.Size = new Size(237, 23);
         txtUserName.TabIndex = 1;
         // 
-        // txtmatkhau
+        // txtpassword
         // 
-        txtmatkhau.Location = new Point(112, 86);
-        txtmatkhau.Margin = new Padding(3, 4, 3, 4);
-        txtmatkhau.Name = "txtmatkhau";
-        txtmatkhau.Size = new Size(270, 27);
-        txtmatkhau.TabIndex = 3;
+        txtpassword.Location = new Point(98, 64);
+        txtpassword.Name = "txtpassword";
+        txtpassword.Size = new Size(237, 23);
+        txtpassword.TabIndex = 3;
         // 
         // label2
         // 
         label2.AutoSize = true;
-        label2.Location = new Point(34, 90);
+        label2.Location = new Point(30, 68);
         label2.Name = "label2";
-        label2.Size = new Size(70, 20);
+        label2.Size = new Size(57, 15);
         label2.TabIndex = 2;
-        label2.Text = "Mật khẩu";
+        label2.Text = "Password";
+        // 
+        // btnlogin
+        // 
+        btnlogin.Location = new Point(98, 107);
+        btnlogin.Name = "btnlogin";
+        btnlogin.Size = new Size(237, 23);
+        btnlogin.TabIndex = 4;
+        btnlogin.Text = "login";
+        btnlogin.UseVisualStyleBackColor = true;
         // 
         // FrmLogin
         // 
-        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(426, 236);
-        Controls.Add(txtmatkhau);
+        ClientSize = new Size(373, 177);
+        Controls.Add(btnlogin);
+        Controls.Add(txtpassword);
         Controls.Add(label2);
         Controls.Add(txtUserName);
         Controls.Add(label1);
+        Margin = new Padding(3, 2, 3, 2);
         Name = "FrmLogin";
         Text = "Đăng nhập";
         ResumeLayout(false);
@@ -87,6 +97,7 @@ partial class FrmLogin
 
     private Label label1;
     private TextBox txtUserName;
-    private TextBox txtmatkhau;
+    private TextBox txtpassword;
     private Label label2;
+    private Button btnlogin;
 }
